17d852fb23341cd8fe4e8777cb5f94489bd22abe
Jeff Reiher <jreiher2003@yahoo.com>
2016-09-10 04:35:03 +0000

    Add urls to NFL app

--- src/nfl/urls.py
+++ src/nfl/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^home/$', views.nfl_home, name='nfl_home'),
+    url(r'^home/$', views.home, name='index'),
+    url(r'^schedule/$', views.nfl_schedule, name='nfl_home'),
     url(r'^board/$', views.nfl_public_board, name='nfl_public_board'),
     url(r'^picked/$', views.nfl_picked_bet, name='nfl_picked_bet'),
     url(r'^confirm/$', views.nfl_confirm_bet, name='nfl_confirm_bet'),
