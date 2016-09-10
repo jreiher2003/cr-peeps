a74a5c53bcf195b753ad7025c249a39a07f54701
Jeff Reiher <jreiher2003@yahoo.com>
2016-09-10 04:34:44 +0000

    Add basic views to project

--- src/nfl/views.py
+++ src/nfl/views.py
@@ -1,13 +1,19 @@
+import json
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
+with open('Schedule.2016.json') as data_file:    
+        data = json.load(data_file)
 
-def index(request):
-    return HttpResponse("Hello, world. NFL home page.")
+def home(request):
+    # for d in data:
+    #     print d["GameKey"]
+    return HttpResponse("Hello, world. NFL home page.%s" % [d["GameKey"] for d in data])
 
-def nfl_home(request):
-    return HttpResponse("nfl_home")
+def nfl_schedule(request):
+
+    context = {"data": data}
+    return render(request, "nfl_schedule.html", context)
 
 def nfl_public_board(request):
     return HttpResponse("nfl_public_board")
@@ -19,5 +25,5 @@ def nfl_confirm_bet(request):
     return HttpResponse("nfl_confirm_bet")
 
 def nfl_create_bet(request):
-    return HttpResponse("nfl_create_bet")
+    return render(request, "nfl_create_bet.html", {})
 
